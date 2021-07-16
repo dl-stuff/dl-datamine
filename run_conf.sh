bdee50a5be5355ae734b8ac3d038965098b5ee76
mushymato <wildshinobu@gmail.com>
2021-07-16 10:23:56 -0400

    do not pre-encode

--- run_conf.sh
+++ run_conf.sh
@@ -4,7 +4,7 @@ python exporter/AdvConf.py -d all
 python exporter/AdvConf.py -wp all
 python exporter/AdvConf.py -w all
 python exporter/AdvConf.py -w base
-python exporter/AdvConf.py -aura all
+python exporter/AdvConf.py -amp all
 python Merge_Conf.py -name skillshare
 python Merge_Conf.py -name exability
 python Merge_Conf.py -name wyrmprints
