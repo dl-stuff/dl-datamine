e2a6c474507a8360fe955b30f75a5ff762397317
mushymato <wildshinobu@gmail.com>
2021-11-16 10:44:37 -0500

    remove skillshare merge

--- run_conf.sh
+++ run_conf.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 python Generate_Conf.py
-python Merge_Conf.py -name skillshare
 python Merge_Conf.py -name exability
 python Merge_Conf.py -name wyrmprints
 python Merge_Conf.py -name amp
